c1e079a764cdf91b317f53ca659ab5010038a41c
Connor Bourke <connorbourke@live.com>
2018-06-23 15:05:09 -0700

    working on teams

--- models/location.js
+++ models/location.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 const User = require('./user.js');
 
 class LocationHandlers {
@@ -9,28 +9,28 @@ class LocationHandlers {
 
     try {
       let code = randomstring.generate(6);
-      let sqla = `select * from team where code = ?`;
+      let sqla = 'select * from team where code = ?';
       while (![await global.db.query(sqla, code)].length) {
         code = randomstring.generate(6);
       }
 
-      let dateObj = new Date();
+      const dateObj = new Date();
       let billingDay = dateObj.getUTCDate();
       if (billingDay > 28) billingDay = 28;
 
-      sqla = `insert into team (name, code, status, billingDay) values (?, ?, 1, ?)`;
-      const [res] = await global.db.query(sqla, [name, code, billingDay]);
+      sqla = 'insert into team (name, code, status, billingDay) values (?, ?, 1, ?)';
+      const [res] = await global.db.query(sqla, [ name, code, billingDay ]);
       const teamID = res.insertId;
 
       if (teamID) {
-        sqla = `update user set teamID = :teamID where id = :userID`;
-        await global.db.query(sqla, {userID: user.id, teamID: teamID});
+        sqla = 'update user set teamID = :teamID where id = :userID';
+        await global.db.query(sqla, { userID: user.id, teamID: teamID });
 
-        sqla = `insert into teamManager (teamID, userID) values (:teamID, :userID)`;
-        await global.db.query(sqla, {userID: user.id, teamID: teamID});
+        sqla = 'insert into teamManager (teamID, userID) values (:teamID, :userID)';
+        await global.db.query(sqla, { userID: user.id, teamID: teamID });
 
-        // await calcTeamMonth(teamID);
-        ctx.body = {teamID: teamID};
+                // await calcTeamMonth(teamID);
+        ctx.body = { teamID: teamID };
       } else {
         ctx.status = 500;
       }
@@ -51,8 +51,8 @@ class LocationHandlers {
       const userID = ctx.params.userID;
       const user = ctx.state.user ? await User.get(ctx.state.user.id) : null;
 
-      const sqla = `update user set locationID = ? where id = ?`;
-      const res = await global.db.query(sqla, [locationID, user.id]);
+      const sqla = 'update user set locationID = ? where id = ?';
+      const res = await global.db.query(sqla, [ locationID, user.id ]);
 
     } catch (e) {
       switch (e.code) {
@@ -66,19 +66,30 @@ class LocationHandlers {
   }
 
   static async getLocationInfo(ctx, locationID = 0) {
+
+    console.log('Getting Location Info');
+
     const user = ctx.state.user ? await User.get(ctx.state.user.id) : null;
 
-    // use same ternary operator as above on locationID and user.locationID
+        // use same ternary operator as above on locationID and user.locationID
+    const loc = ctx.state.user ? await User.get(ctx.state.user.locationID) : null;
 
-    // Use sql to find a list of all users in your same location.
-    // Loop through them and...
-    //   User.scrapeUser(ctx, userID)
-    //   Create an array of userIDs  use push to an array defined above
+        // Use sql to find a list of all users in your same location.
+    const [[localUsers]] = await global.db.query(
+            `SELECT *
+             FROM user
+             WHERE locationID = :locationID`,
+          { locationID: 1 }
+      );
+    console.log(localUsers);
+        // Loop through them and...
+        //   User.scrapeUser(ctx, userID)
+        //   Create an array of userIDs  use push to an array defined above
 
-    // Use array.join to make a comma separated list of userIDs
+        // Use array.join to make a comma separated list of userIDs
 
-    // Find the count(*) of the userChallenge where cDate > (user moment to calculate a month ago) and userID in ([comma separated list of userIDs])
-    // Find the count(*) of the userChallenge where cDate > (user moment to calculate a week ago) and userID in ([comma separated list of userIDs])
+        // Find the count(*) of the userChallenge where cDate > (user moment to calculate a month ago) and userID in ([comma separated list of userIDs])
+        // Find the count(*) of the userChallenge where cDate > (user moment to calculate a week ago) and userID in ([comma separated list of userIDs])
 
   }
 
