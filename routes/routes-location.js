e13d6e1850982535225882505ec42f7b391a6c2b
anyakov <akovneva@student.yosemite.edu>
2018-06-23 14:32:18 -0700

    worked on routes

--- routes/routes-location.js
+++ routes/routes-location.js
@@ -1,29 +1,30 @@
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
-/*  Teams routes                                                                                  */
+/*  Location routes                                                                                  */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
 'use strict';
 
 const router = require('koa-router')(); // router middleware for koa
-const team = require('../models/team.js');
+const location = require('../models/location.js');
 
-router.get('/admin/teams', team.adminGetTeams);
+//router.get('/admin/teams', team.adminGetTeams);
 
-router.put('/team/edit', team.updateTeamMember);
-router.get('/team/:teamID?', team.getTeam);
-router.post('/team', team.addTeam);
-router.put('/team/:teamID', team.updateTeam);
-router.delete('/team/:teamID', team.removeTeam);
+//router.put('/team/edit', team.updateTeamMember);
+//router.get('/team/:teamID?', team.getTeam);
+router.post('/location', location.addLocation);
+//router.put('/team/:teamID', team.updateTeam);
+//router.delete('/team/:teamID', team.removeTeam);
 
-router.post('/team/:teamID/member', team.addTeamMember);
-router.delete('/team/:teamID/member/:memberID', team.disableTeamMember);
-router.put('/team/:teamID/member/:memberID', team.enableTeamMember);
-router.delete('/team/:teamID/member/:memberID/remove', team.removeTeamMember);
+router.post('/location/:locationID/member', location.addUserToLocation);
+//router.delete('/team/:teamID/member/:memberID', team.disableTeamMember);
+//router.put('/team/:teamID/member/:memberID', team.enableTeamMember);
+//router.delete('/team/:teamID/member/:memberID/remove', team.removeTeamMember);
+router.get('/location/getLocation', location.getLocationInfo);
 
-router.post('/team/:teamID/invite', team.inviteToTeam);
-router.put('/team/:teamID/removeInvite', team.removeInvite);
+//router.post('/team/:teamID/invite', team.inviteToTeam);
+//router.put('/team/:teamID/removeInvite', team.removeInvite);
 
-router.put('/team/:teamID/makeAdmin/:userID/:isAdmin', team.toggleTeamAdmin);
+//router.put('/team/:teamID/makeAdmin/:userID/:isAdmin', team.toggleTeamAdmin);
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
